ae875224d817fd8273d8bb53033b085db52575bf
Alexei Shcherbakov <falcon.mk2@gmail.com>
2015-08-03 14:41:47 +0300

    Баг - https://connect.microsoft.com/VisualStudio/feedback/details/1621500

---
+++ Issue1621500/EditAndContinueTestbench/Program.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditAndContinueTestbench
+{
+	[TestFixture]
+	public class Program
+	{
+
+		static void Main(string[] args)
+		{
+			var program = new Program();
+
+			program.Test();
+        }
+
+		[Test]
+		public void Test()
+		{
+			var methods = new Methods();
+			Test(methods);
+        }
+
+		public void Test(ITest test)
+		{
+			// 1
+			test.MethodParamsString("a", "a", "a");
+			// 2
+			test.MethodParamsString("b", "b", "b", "b");
+			// 3
+			test.MethodParamsString("c", "c", "c", "c");
+		}
+	}
+
+
+	public interface ITest
+	{
+		ITest MethodString(string item);
+    }
+
+
+	public static class TestExtensions
+	{
+		public static ITest MethodParamsString(this ITest obj,params string[] items)
+		{
+			Random r = new Random(Environment.TickCount);
+			StringBuilder b = new StringBuilder();
+
+			for (int i = 0; i < items.Length * 2; i++)
+			{
+				b.Append(items[r.Next(0, items.Length)]);
+			}
+			return obj.MethodString(b.ToString());
+        }
+    }
+
+	public class Methods
+		: ITest
+	{
+		public ITest MethodString(string item)
+		{ 
+			Debug.WriteIf(Environment.TickCount > 0x0FFFFFFF, item);
+
+			return this;
+		}
+	}
+}
